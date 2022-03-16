deda38dad9d1388cf4393f7e44dab2d895f643ca
sachinabs <sachinabs.js@gmail.com>
2022-03-16 21:35:46 +0530

    first commit

---
+++ runme.sh
@@ -0,0 +1,8 @@
+echo "# google-single-sign-on" >> README.md
+git init
+git add README.md
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sachinabs/google-single-sign-on.git
+git push -u origin main
